59b3c673174a9dce01b406563db1e5aa4cde1612
Bruno <bbartolomeu.net@gmail.com>
2021-07-17 18:43:12 -0300

    Alterando serverjs

--- Api-mybudget/server.js
+++ Api-mybudget/server.js
@@ -1,6 +1,10 @@
 
 const express = require('express')
 const Custo = require("./models/Custo")
+const Custo = require("./models/Orcamento")
+const Custo = require("./models/Login")
+const Custo = require("./models/Cadastro")
+const Orcamento = require('./models/Orcamento')
 const app = express()
 const port = 3000
 app.use(express.json());
@@ -24,6 +28,67 @@ app.post('/custos', async (req, res)=> {
     console.log(err);
     }
     });
+
+
+    app.get('/Orcamento', async (req, res) => {
+      try {
+         const Orcamento = await Orcamento.find()
+         return res.send({ Orcamento })
+      } catch(err)
+      {
+        console.log(err);
+      }
+    })
+    
+    app.post('/Orcamento', async (req, res)=> {
+        try{
+        const Orcamento = await Orcamento.create({...req.body});
+        return res.send({ Orcamento });
+        } catch(err){
+        console.log(err);
+        }
+        });
+
+
+      app.get('/Login', async (req, res) => {
+          try {
+             const Login = await Login.find()
+             return res.send({ Login })
+          } catch(err)
+          {
+            console.log(err);
+          }
+        })
+        
+        app.post('/Login', async (req, res)=> {
+            try{
+            const Login = await Login.create({...req.body});
+            return res.send({ Login });
+            } catch(err){
+            console.log(err);
+            }
+            });
+
+            
+            app.get('/Cadastro', async (req, res) => {
+              try {
+                 const Cadastro = await Custo.find()
+                 return res.send({ Cadastro })
+              } catch(err)
+              {
+                console.log(err);
+              }
+            })
+            
+            app.post('/Cadastro', async (req, res)=> {
+                try{
+                const Cadastro = await Custo.create({...req.body});
+                return res.send({ Cadastro });
+                } catch(err){
+                console.log(err);
+                }
+                });
+
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
